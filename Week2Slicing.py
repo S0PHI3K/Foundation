189eeb87f2f6409394d3a66773a1e228dc9e60cc
Sophie <107502956+S0PHI3K@users.noreply.github.com>
2022-09-02 14:27:11 +0100

    Create Week2Slicing.py

---
+++ Week2Slicing.py
@@ -0,0 +1,14 @@
+#Some examples of slicing
+
+# Task 2 - Slice the word until "o". (Pyth)
+wrd="Python"
+# Type your answer here.
+ans_1= wrd[0:4]
+print(ans_1)
+
+# Task 4 - Now slice the word with steps of 2, excluding first and last 
+characters
+wrd="Python"
+# Type your answer here.
+ans_1= wrd[1:5:2]
+print(ans_1)
