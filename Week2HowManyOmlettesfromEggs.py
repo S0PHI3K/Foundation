5e73edfbd00a8598e7a3c98430376f0d96a2ab4b
Sophie <107502956+S0PHI3K@users.noreply.github.com>
2022-09-02 14:24:17 +0100

    Create Week2HowManyOmlettesfromEggs.py

---
+++ Week2HowManyOmlettesfromEggs.py
@@ -0,0 +1,9 @@
+
+Number_of_eggs = int(input("How many boxes containing 6 eggs do you have?"))
+
+box_of_eggs = 6
+omlette = 4
+number_of_omlettes = (number_of_eggs * box_of_eggs) / omlette
+
+print(f"You can make {int(number_of_omlettes)} omlettes with {number_of_eggs} boxes 
+of eggs")
